b3c311d4dafb394a9ab500d206cea5b755a83f86
MohamedYatera <yateramohamed25@gmail.com>
2024-06-10 18:11:04 -0400

    Added failing test for subtraction

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -9,4 +9,11 @@ public class CalculatorTest {
         int result = calculator.add(10, 20);
         assertEquals(30, result);
     }
+
+    @Test
+    public void testSubtract() {
+        Calculator calculator = new Calculator();
+        int result = calculator.subtract(20, 10);
+        assertEquals(10, result);
+    }
 }
